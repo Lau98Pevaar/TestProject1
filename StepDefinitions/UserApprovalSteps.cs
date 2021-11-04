d3e5965fdb2d5adae6209ca1247a4fe1dcd9d9ba
Lau98Pevaar <laura.moreno@pevaar.com>
2021-11-04 17:38:15 -0500

    help

--- StepDefinitions/UserApprovalSteps.cs
+++ StepDefinitions/UserApprovalSteps.cs
@@ -11,19 +11,6 @@ namespace TestProject1.StepDefinitions
     {
         UserApproval Userapproval = new UserApproval();
 
-        [When(@"Click on My Profile Menu")]
-        public void WhenClickOnMyProfileMenu()
-        {
-            Userapproval.MyProfileMenu.Click();
-        }
-        
-        [When(@"Switch to admin")]
-        public void WhenSwitchToAdmin()
-        {
-            Userapproval.SwitchToAdmin.Click();
-            Thread.Sleep(10);
-            //Assert.That(Userapproval.WelcomeToAdminPage, Is.True, "Welcome to Admin Page did not displayed");
-        }
         
         [When(@"Select Admin Component")]
         public void WhenSelectAdminComponent()
@@ -37,14 +24,14 @@ namespace TestProject1.StepDefinitions
         {
 
             Userapproval.UserApprovalsOption.Click();
-            
-            Assert.That(Userapproval.WelcomeToAdminPage, Is.True, "/html/body/app-root/app-dashboard/div/section[1]/div/app-approvals/app-users-approval/div[2]");
+            Thread.Sleep(30);
+            //Assert.That(Userapproval.WelcomeToAdminPage, Is.True, "/html/body/app-root/app-dashboard/div/section[1]/div/app-approvals/app-users-approval/div[2]");
         }
         [When(@"Search the user recently registered")]
         public void WhenSearchTheUserRecentlyRegistered()
         {
-            Userapproval.SearchInput.Click();
-            Userapproval.SearchName();
+            Userapproval.SearchName("Laura");
+            Thread.Sleep(3000);
         }
 
     }
